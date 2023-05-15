0a9cfb5997f9044acf4100afc07b3f4fd990f085
devvaditya <devvaditya14@gmail.com>
2023-05-15 22:48:11 +0530

    This is is the second project that calculates BMI

---
+++ Project 2.py
@@ -0,0 +1,4 @@
+# BMI Calculator
+height = input("Enter Your Height in m: ")
+weight = input("Enter your Weight in kgs: ")
+print("Your BMI(Body Mass Index) is : %.f" % (float(weight)/(float(height) ** 2)))
