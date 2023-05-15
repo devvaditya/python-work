4bace1835cfc609e7198ec2fcb1a8e80af311dec
Aditya Kumar Singh <devvaditya14@gmail.com>
2023-05-15 23:55:08 +0530

    The Fourth project to my learning that shows life in weeks

---
+++ Project 4.py
@@ -0,0 +1,6 @@
+#Your Life In Weeks!
+age = int(input("What is Current Age? \n"))
+days = (90 - age) * 365
+weeks = (90 - age) * 52
+months = (90 - age) * 12
+print(f"Assuming you would live till 90 years of age, You have {days} days, {weeks} weeks and {months} left.\nWe never what would happen next so live your life to the fullest and make every moment count.")
